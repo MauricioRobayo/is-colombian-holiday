639c963982f0f47a2c617298d1eb6ed010a4c198
Mauricio Robayo <rfmajo@gmail.com>
2023-02-19 21:17:25 -0500

    always render date

--- src/components/holidays-list.tsx
+++ src/components/holidays-list.tsx
@@ -21,6 +21,7 @@ export function HolidaysList({ year, month }: HolidaysListProps) {
     : allHolidays;
   const today = new Date();
   const currentYear = today.getUTCFullYear();
+  const isCurrentYear = currentYear === year;
   return (
     <ol className="flex flex-col gap-4">
       {holidays.map((holiday) => {
@@ -30,18 +31,16 @@ export function HolidaysList({ year, month }: HolidaysListProps) {
             <div
               className={twMerge(
                 "text-md sm:text-lg",
-                alreadyOver && "text-slate-400"
+                isCurrentYear && alreadyOver && "text-slate-400"
               )}
             >
               {holiday.name.en}
             </div>
             <time dateTime={holiday.celebrationDate.toISOString()}>
-              {!alreadyOver && (
-                <div className={"text-xl sm:text-2xl"}>
-                  {longDateFormatter.format(holiday.celebrationDate)}
-                </div>
-              )}
-              {currentYear === year && (
+              <div className={"text-xl sm:text-2xl"}>
+                {longDateFormatter.format(holiday.celebrationDate)}
+              </div>
+              {isCurrentYear && (
                 <div className="text-sm text-slate-400 sm:text-base">
                   {formatDistanceToNow(holiday.celebrationDate)}
                 </div>
