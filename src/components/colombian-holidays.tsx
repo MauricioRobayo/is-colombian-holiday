91cb407185f236a8353b21c1a38f2c701c444286
Mauricio Robayo <rfmajo@gmail.com>
2023-02-20 08:19:58 -0500

    enable month navigation

--- src/components/colombian-holidays.tsx
+++ src/components/colombian-holidays.tsx
@@ -12,7 +12,6 @@ interface ColombianHolidaysProps {
 export function ColombianHolidays({
   year,
   month: selectedMonth,
-  day,
 }: ColombianHolidaysProps) {
   const previousYear = year - 1;
   const nextYear = year + 1;
@@ -23,7 +22,13 @@ export function ColombianHolidays({
         <Link className="text-lg text-violet-400" href={String(previousYear)}>
           &larr; {previousYear}
         </Link>
-        <h2>{year}</h2>
+        {selectedMonth ? (
+          <Link href={`/${String(year)}`}>
+            <h2>{year}</h2>
+          </Link>
+        ) : (
+          <h2>{year}</h2>
+        )}
         <Link className="text-lg text-violet-400" href={String(nextYear)}>
           {nextYear} &rarr;
         </Link>
@@ -31,10 +36,13 @@ export function ColombianHolidays({
       <div className="mt-4 grid grid-cols-3">
         {months.map((month, i) =>
           i + 1 === selectedMonth ? (
-            month
+            <div key={month} className="font-bold">
+              {month}
+            </div>
           ) : (
             <Link
               key={month}
+              className="text-violet-400"
               href={`${year}/${String(i + 1).padStart(2, "0")}`}
             >
               {month}
