6406c2c3d59a8eec72097bbbc0d62bc29ee0dae8
Mauricio Robayo <rfmajo@gmail.com>
2023-03-06 15:12:13 -0500

    chore(#13): year list styling

--- src/components/years-list.tsx
+++ src/components/years-list.tsx
@@ -1,3 +1,4 @@
+import cn from "clsx";
 import { getYears } from "@/utils/get-years";
 import { Link } from "@/components/link";
 
@@ -6,15 +7,31 @@ interface YearsListProp {
   selectedYear?: number;
 }
 export function YearsList({ years = 25, selectedYear }: YearsListProp) {
+  const today = new Date();
   return (
     <div className="grid grid-cols-5 gap-x-4 gap-y-2">
       {getYears(years).map((year) => {
+        const isCurrentYear = today.getUTCFullYear() === year;
+        const isSelectedYear = year === selectedYear;
+        console.log(isCurrentYear);
         return (
           <div key={year}>
-            {year === selectedYear ? (
-              <div className="text-md font-bold">{year}</div>
+            {isSelectedYear ? (
+              <div
+                className={cn(
+                  "text-slate-400",
+                  isCurrentYear && "text-lg font-bold"
+                )}
+              >
+                {year}
+              </div>
             ) : (
-              <Link href={`/${year}`}>{year}</Link>
+              <Link
+                href={`/${year}`}
+                className={cn(isCurrentYear && "text-lg font-bold")}
+              >
+                {year}
+              </Link>
             )}
           </div>
         );
