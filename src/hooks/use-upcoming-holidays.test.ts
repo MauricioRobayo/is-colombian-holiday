97d6130e57c176d06088463915b0cef5e67869fa
Mauricio Robayo <rfmajo@gmail.com>
2023-03-13 16:23:55 -0500

    test(#39): getUpcomingHolidays

--- src/hooks/use-upcoming-holidays.test.ts
+++ src/hooks/use-upcoming-holidays.test.ts
@@ -1,9 +1,25 @@
 import { getUpcomingHolidays } from "./use-upcoming-holidays";
 
-it("should return the list of upcoming holidays", () => {
+beforeEach(() => {
   jest.useFakeTimers();
+});
+
+afterAll(() => {
+  jest.useRealTimers();
+});
+
+it("should not include today if today is a holiday", () => {
   jest.setSystemTime(new Date("2023-03-20T00:00:00.000Z"));
   const upcoming = getUpcomingHolidays();
-  console.log(upcoming);
+  const [next] = upcoming;
   expect(upcoming.length).toBe(4);
+  expect(next.celebrationDate.toISOString()).toBe("2023-04-06T00:00:00.000Z");
+});
+
+it("should return the list of the next 4 upcoming holidays", () => {
+  jest.setSystemTime(new Date("2023-03-19T00:00:00.000Z"));
+  const upcoming = getUpcomingHolidays();
+  const [next] = upcoming;
+  expect(upcoming.length).toBe(4);
+  expect(next.celebrationDate.toISOString()).toBe("2023-03-20T00:00:00.000Z");
 });
