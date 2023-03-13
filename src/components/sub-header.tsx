862ba13fd5c20b3fd7b509e710f9ed9f6ea0a104
Mauricio Robayo <rfmajo@gmail.com>
2023-03-12 21:30:08 -0500

    chore: remove today's date

--- src/components/sub-header.tsx
+++ src/components/sub-header.tsx
@@ -14,30 +14,18 @@ interface SubHeaderProps {
 export function SubHeader({ breadcrumbs, className = "" }: SubHeaderProps) {
   const date = new Date();
   return (
-    <>
-      <div className={twMerge(" bg-orange-200 text-sm", className)}>
-        <Wrapper>
-          <p>In Colombia today is</p>
-          <Link href={formatDateAsPath(date)} title="Today" className="text-sm">
-            <time dateTime={date.toISOString()}>
-              {longDateFormatter.format(date)}
-            </time>
-          </Link>
-        </Wrapper>
-      </div>
-      <div
-        className={twMerge(
-          "border-b-2 border-slate-300 bg-slate-200 text-sm",
-          className
-        )}
-      >
-        <Wrapper className="align-center flex justify-center gap-4 pt-3 pb-2">
-          <Breadcrumbs breadcrumbs={breadcrumbs} />
-          <ShareButton variant="icon" className="flex items-center gap-1">
-            Share <HiOutlineShare />
-          </ShareButton>
-        </Wrapper>
-      </div>
-    </>
+    <div
+      className={twMerge(
+        "border-b-2 border-t-4 border-orange-400  bg-orange-200 text-sm",
+        className
+      )}
+    >
+      <Wrapper className="align-center flex justify-center gap-4 pt-3 pb-2">
+        <Breadcrumbs breadcrumbs={breadcrumbs} />
+        <ShareButton variant="icon" className="flex items-center gap-1">
+          Share <HiOutlineShare />
+        </ShareButton>
+      </Wrapper>
+    </div>
   );
 }
